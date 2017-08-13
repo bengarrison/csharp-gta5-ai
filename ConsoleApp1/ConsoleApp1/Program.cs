a6cab16a73a0c03f71a5908c3508176845824e41
Ben Garrison <ben@64bytes.com>
2017-08-13 09:48:11 -0700

    continued working with image filters

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Drawing.Text;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -109,24 +110,61 @@ namespace ConsoleApp1
             g.CopyFromScreen(captureArea.Left, captureArea.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
 
             var src = OpenCvSharp.Extensions.BitmapConverter.ToMat(bmp);
-            var dst = new Mat();
-            src.CopyTo(dst);
-            //src = src.GaussianBlur(new OpenCvSharp.Size(7, 9),0);
-            src = src.CvtColor(ColorConversionCodes.BGR2GRAY);
-            src = src.Canny(50, 75);
+            var vertices = new List<List<Point>>
+            {
+                new List<Point>() {new Point(10, 500)},
+                new List<Point>() {new Point(10, 300)},
+                new List<Point>() {new Point(300, 200)},
+                new List<Point>() {new Point(500, 200)},
+                new List<Point>() {new Point(700, 300)},
+                new List<Point>() {new Point(700, 500)}
+            };
 
-            var lines = src.HoughLinesP(1, Math.PI / 180, 50);
+            //src = src.CvtColor(ColorConversionCodes.RGB2RGBA);
+
+            src.FillPoly(vertices, Scalar.Pink);
+            src.AdjustROI(200, 200, 200, 200);
+            src = src.CvtColor(ColorConversionCodes.BGR2GRAY);
+            src = src.Canny(200, 300);
+            src = src.GaussianBlur(new OpenCvSharp.Size(5,5), 0);
+
+            var lines = src.HoughLinesP(1, Math.PI / 180, 50, 30, 15);
+
+            var dst = src.EmptyClone();
+
+            
+            foreach (var line in lines)
+                Cv2.Line(dst, line.P1, line.P2, Scalar.Cyan, 2);
+
+            
+            return OpenCvSharp.Extensions.BitmapConverter.ToBitmap(dst);
+
+            //var mask = src.EmptyClone();
+
+
+            //Cv2.BitwiseAnd(src, mask, OutputArray.Create(src));
+
+
+            //var dst = new Mat();
+            //src.CopyTo(dst);
+            //src = src.GaussianBlur(new OpenCvSharp.Size(7, 9),0);
+            //src = src.CvtColor(ColorConversionCodes.BGR2GRAY);
+            //src = src.Canny(50, 75);
+            //var channels=src.Split();
+
+
+            //var lines = src.HoughLinesP(1, Math.PI / 180, 50);
             //var Blank = new Mat(src.Rows, src.Cols, MatType.CV_8UC3, new Scalar(0, 0, 0));
 
-            foreach (var line in lines)
-            {
-                //Cv2.Line(src, line.P1, line.P2, new Scalar(0, 0, 0), 2, LineTypes.AntiAlias);
-                Cv2.Line(dst, line.P1, line.P2, new Scalar(255, 255, 255), 2, LineTypes.AntiAlias);
-            }
+            //foreach (var line in lines)
+            //{
+            //    //Cv2.Line(src, line.P1, line.P2, new Scalar(0, 0, 0), 2, LineTypes.AntiAlias);
+            //    Cv2.Line(dst, line.P1, line.P2, new Scalar(255, 255, 255), 2, LineTypes.AntiAlias);
+            //}
 
             //src = src.AdjustROI(100, 640, 100, 700);
 
-            return OpenCvSharp.Extensions.BitmapConverter.ToBitmap(dst);
+
         }
     }
 }
